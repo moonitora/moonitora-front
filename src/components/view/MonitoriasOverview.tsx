903f541d0268a74c145b77fe37b6c1315e877a93
Victor Betoni <victorbetoni@protonmail.com>
2022-08-18 11:05:10 -0300

    Corrigindo <p> no lugar errado

--- src/components/view/MonitoriasOverview.tsx
+++ src/components/view/MonitoriasOverview.tsx
@@ -29,43 +29,45 @@ export default function MonitoriasOverview() {
         <div>
             <Navbar/>
             {monitorias !== null ?
-                <div className="grid-cols-3 grid gap-4 mx-16">
-                    {monitorias.map((monitoria) => {
+                <div>
+                    <p className="text-2xl font-light py-10 px-10">Minhas monitorias</p>
+                    <div className="grid-cols-3 grid gap-4 mx-16">
+                        {monitorias.map((monitoria) => {
 
-                        const data = monitoria.monitoria.data.split('-');
+                            const data = monitoria.monitoria.data.split('-');
 
-                        return (
-                            <div className="font-inter text-gray-700 border-[1px] border-moonitora-cyan p-3 rounded-lg">
-                                <p className="text-2xl font-light py-10 px-10">Minhas monitorias</p>
-                                <p className="text-md">{monitoria.monitoria.conteudo}</p>
-                                <p className="text-xs mb-4">{monitoria.monitoria.disciplina}</p>
-                                <p className="text-xs mb-4"><b>{data[2]}/{data[1]}/{data[0]}</b> {translate(monitoria.horario)}</p>
-                                <p className="text-xs">Aluno: {monitoria.monitoria.aluno_nome} - {monitoria.monitoria.aluno_ra}</p>
-                                <p className="text-xs mb-4">Marcada por: {monitoria.monitoria.marcada_por}</p>
-                                {monitoria.monitoria.status === 0 ? <p className="text-xs mb-4">Status: <span className="text-yellow-400 font-bold">Aguardando confirmação</span></p>
-                                    : monitoria.monitoria.status === 1 ?<p className="text-xs mb-4">Status: <span className="text-green-400 font-bold">Confirmada</span></p>
-                                        : <p className="text-xs mb-4">Status: <span className="text-red-500">Cancelada</span></p>}
-                                <div className="justify-between flex">
-                                    <div></div>
-                                    <div>
-                                        {monitoria.monitoria.status === 0 ?
-                                            <div className="flex gap-3">
-                                                <p className="text-sm font-bold text-red-400 hover:cursor-pointer">Cancelar</p>
-                                                <p className="text-sm font-bold text-green-400 hover:cursor-pointer">Confirmar</p>
-                                            </div>
-                                            : monitoria.monitoria.status === 1 ?
-                                                <div>
+                            return (
+                                <div className="font-inter text-gray-700 border-[1px] border-moonitora-cyan p-3 rounded-lg">
+                                    <p className="text-md">{monitoria.monitoria.conteudo}</p>
+                                    <p className="text-xs mb-4">{monitoria.monitoria.disciplina}</p>
+                                    <p className="text-xs mb-4"><b>{data[2]}/{data[1]}/{data[0]}</b> {translate(monitoria.horario)}</p>
+                                    <p className="text-xs">Aluno: {monitoria.monitoria.aluno_nome} - {monitoria.monitoria.aluno_ra}</p>
+                                    <p className="text-xs mb-4">Marcada por: {monitoria.monitoria.marcada_por}</p>
+                                    {monitoria.monitoria.status === 0 ? <p className="text-xs mb-4">Status: <span className="text-yellow-400 font-bold">Aguardando confirmação</span></p>
+                                        : monitoria.monitoria.status === 1 ?<p className="text-xs mb-4">Status: <span className="text-green-400 font-bold">Confirmada</span></p>
+                                            : <p className="text-xs mb-4">Status: <span className="text-red-500">Cancelada</span></p>}
+                                    <div className="justify-between flex">
+                                        <div></div>
+                                        <div>
+                                            {monitoria.monitoria.status === 0 ?
+                                                <div className="flex gap-3">
                                                     <p className="text-sm font-bold text-red-400 hover:cursor-pointer">Cancelar</p>
+                                                    <p className="text-sm font-bold text-green-400 hover:cursor-pointer">Confirmar</p>
                                                 </div>
-                                                :
-                                                <div>
-                                                    <p className="text-sm font-bold text-red-400 hover:cursor-pointer">Cancelar</p>
-                                                </div>}
+                                                : monitoria.monitoria.status === 1 ?
+                                                    <div>
+                                                        <p className="text-sm font-bold text-red-400 hover:cursor-pointer">Cancelar</p>
+                                                    </div>
+                                                    :
+                                                    <div>
+                                                        <p className="text-sm font-bold text-red-400 hover:cursor-pointer">Cancelar</p>
+                                                    </div>}
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        )
-                    })}
+                            )
+                        })}
+                    </div>
                 </div>
             :
                 <div className="flex justify-center font-inter ">
